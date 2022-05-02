ceb7a6d42dd62aaf4d30bfc63879d8771e88b1af
Kevin Arellano <103948578+Kevin-Arellano94@users.noreply.github.com>
2022-05-02 15:49:50 -0400

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,3 +1,6 @@
+# https://raw.githubusercontent.com/Kevin-Arellano94/documentation/main/workflow.py
+# https://www.btelligent.com/en/blog/best-practice-working-with-paths-in-python-part-1/
+
 import glob
 
 # Variables that are called
@@ -7,11 +10,13 @@ repo = r'/documentation'
 branch = r'/main'
 
 # Paths
+my_path = r"C:/Users/kevin/Documents/GitHub/eHawk-Inc"
 my_github_path = r"D:\a\documentation-action\documentation-action"
 
 # Gets all the MarkDown files
 files = glob.glob(
-    my_path +
+    # my_path +
+    my_github_path +
     '*/**/*.md',
     recursive = True
 )
@@ -28,7 +33,7 @@ for items in files:
 
     # For production
     raw_url = new_items.replace(
-        r'D:\a\documentation-action\documentation-action',
+        f'{ my_github_path }',
         domain + user + repo + branch
     )
 
