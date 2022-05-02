d6d0c11fb610d5b6789f920e134e56dd4e06ce3f
Kevin Arellano <103948578+Kevin-Arellano94@users.noreply.github.com>
2022-05-02 15:12:32 -0400

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,5 +1,10 @@
 import glob
 
+domain = r'https:\\raw.githubusercontent.com'
+user = r'\Kevin-Arellano94'
+repo = r'\documentation'
+branch = r'\main'
+
 my_path = r"D:\a\documentation-action\documentation-action"
 
 files = glob.glob(
@@ -9,7 +14,11 @@ files = glob.glob(
 )
 
 for items in files:
-    print(items)
+    raw_url = items.replace(
+        r'D:\a\documentation-action\documentation-action',
+        domain + user + repo + branch
+    )
+    print(raw_url)
 
 
 print(f'completed...')
