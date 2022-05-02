1533c560e850242376638dead4b2d6854eb246ac
Kevin Arellano <103948578+Kevin-Arellano94@users.noreply.github.com>
2022-05-02 14:36:35 -0400

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,8 +1,19 @@
 import os
+import glob
+
 
 hw = "Hello World!"
 print(hw)
 
+my_path = "/Users/kevin/Documents/GitHub/eHawk-Inc"
+
+files = glob.glob(
+    my_path +
+    '*/**/*.md',
+    recursive = True
+)
+
+print(f'{ files }')
 print(f'completed...')
 
 exit(0)
